24ac354e40eadae36075fccbf29a4d0a6806ea23
Imbaxiaocai <imbaxiaocai@foxmail.com>
2021-08-28 00:35:25 +0800

    init project

---
+++ src/main/java/com/cjf/crossafterdubbo/CrossafterDubboApplication.java
@@ -0,0 +1,13 @@
+package com.cjf.crossafterdubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossafterDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossafterDubboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cjf/crossafterdubbo/CrossafterDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjf.crossafterdubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossafterDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
