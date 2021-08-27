aef41086e24fc423abca95b8c81b28fb320e7afb
Imbaxiaocai <imbaxiaocai@foxmail.com>
2021-08-28 01:29:21 +0800

    init module: user

---
+++ user/src/main/java/com/cjf/user/bean/User.java
@@ -0,0 +1,11 @@
+package com.cjf.user.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userId;
+    private String userName;
+    private String mobilePhone;
+    private String passwd;
+}
